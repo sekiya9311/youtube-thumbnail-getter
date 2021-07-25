96ef312d2c55700b7e8862efa79f8923b65c5a86
sekiya9311 <sekiya9311@gmail.com>
2021-07-25 11:49:07 +0900

    feat: impl api

---
+++ pages/api/video.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Item, Videos } from '../../models/Videos';
+
+const VIDEOS_API_URL_ENDPOINT =
+  'https://content-youtube.googleapis.com/youtube/v3/videos';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Item>
+) {
+  const setStatusCodeAndEnd = (statusCode: number) => {
+    res.statusCode = statusCode;
+    res.end();
+  };
+
+  if (req.method !== 'GET') {
+    setStatusCodeAndEnd(400);
+    return;
+  }
+  const videoId = req.query.id;
+  if (typeof videoId !== 'string') {
+    setStatusCodeAndEnd(400);
+    return;
+  }
+
+  const apiUrl = new URL(VIDEOS_API_URL_ENDPOINT);
+  apiUrl.searchParams.set('part', 'snippet');
+  apiUrl.searchParams.set('key', process.env.YOUTUBE_API_KEY!);
+  apiUrl.searchParams.set('id', videoId);
+
+  const apiRes = await fetch(apiUrl.toString());
+  if (apiRes.status !== 200) {
+    setStatusCodeAndEnd(apiRes.status);
+    return;
+  }
+
+  const responseObj = (await apiRes.json()) as Videos;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(responseObj));
+}
