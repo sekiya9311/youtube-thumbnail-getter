ea7968cbca31ec66e804b076d4e484b86358c60e
sekiya9311 <sekiya9311@gmail.com>
2023-05-29 22:08:36 +0900

    feat: 11 to 12

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const withPwa = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const withPwa = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV !== 'production',
+  runtimeCaching
+});
 
 module.exports = withPwa({
   reactStrictMode: true,
   images: {
     domains: ['i.ytimg.com'],
-  },
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
   },
 });
