5ad36b0bc0335a61935af4f08d4ab59a9d024103
sekiya9311 <sekiya9311@gmail.com>
2023-06-20 22:49:18 +0900

    chore: remove unused files

--- app/_api/getImageAsync.ts
+++
@@ -1,8 +0,0 @@
-export const getImageAsync = async (urlText: string) => {
-  const apiUrl = new URL(location.origin);
-  apiUrl.pathname = '/api/image';
-  apiUrl.searchParams.set('url', urlText);
-
-  const response = await fetch(apiUrl.toString());
-  return await response.blob();
-};
