70d246f64618614e2aba9a28fa670dd7c5059181
sekiya9311 <sekiya9311@gmail.com>
2023-06-20 22:41:35 +0900

    feat: add api routes

--- app/_components/DownloadButton.tsx
+++ app/_components/DownloadButton.tsx
@@ -1,8 +1,4 @@
-'use client';
-
-import { useRef } from 'react';
 import { MdFileDownload } from 'react-icons/md';
-import { getImageAsync } from '../_api/getImageAsync';
 
 type Props = {
   loadedThumbnail: boolean;
@@ -15,38 +11,22 @@ export const DownloadButton = ({
   thumbnailUrl,
   videoName,
 }: Props) => {
-  const downloadTag = useRef<HTMLAnchorElement>(null);
-  const downloadThumbnail = async () => {
-    if (!downloadTag.current) {
-      return;
-    }
-
-    const blob = await getImageAsync(thumbnailUrl);
-    const blobUrl = window.URL.createObjectURL(blob);
-    try {
-      downloadTag.current.href = blobUrl;
-      downloadTag.current.download = `${videoName}${thumbnailUrl.substring(
-        thumbnailUrl.lastIndexOf('.')
-      )}`;
-      downloadTag.current.click();
-    } catch (e) {
-      alert(e);
-    } finally {
-      URL.revokeObjectURL(blobUrl);
-    }
-  };
   return (
     <>
-      <button
-        disabled={!loadedThumbnail}
-        className={`flex justify-center items-center w-2/12 bg-red-400 rounded-full text-white text-2xl font-bold p-2 md:p-4 hover:bg-red-600${
-          loadedThumbnail ? '' : ' opacity-25 cursor-not-allowed'
-        }`}
-        onClick={downloadThumbnail}
-      >
-        <MdFileDownload />
-      </button>
-      <a className='hidden' ref={downloadTag} />
+      {loadedThumbnail ? (
+        <a
+          className='flex justify-center items-center w-2/12 bg-red-400 rounded-full text-white text-2xl font-bold p-2 md:p-4 hover:bg-red-600'
+          href={`/api/image?url=${encodeURIComponent(
+            thumbnailUrl
+          )}&name=${encodeURIComponent(videoName)}`}
+        >
+          <MdFileDownload />
+        </a>
+      ) : (
+        <span className='flex justify-center items-center w-2/12 bg-red-400 rounded-full text-white text-2xl font-bold p-2 md:p-4 hover:bg-red-600 opacity-25 cursor-not-allowed'>
+          <MdFileDownload />
+        </span>
+      )}
     </>
   );
 };
--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,10 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const getExt = (url: string): string => {
+  const dot = url.lastIndexOf('.');
+  if (dot === -1) {
+    return '.png';
+  }
+  return url.substring(dot);
+};
+
 export async function GET(request: NextRequest) {
   const imageUrl = request.nextUrl.searchParams.get('url');
+  const name = request.nextUrl.searchParams.get('name');
   if (!imageUrl) {
     return NextResponse.next({ status: 404 });
   }
+  if (!name) {
+    return NextResponse.next({ status: 400 });
+  }
+
+  const ext = getExt(imageUrl);
+  const nameWithExt = `${name}${ext}`;
 
   const fetchResponse = await fetch(imageUrl);
   const buf = await fetchResponse.arrayBuffer();
@@ -15,5 +30,14 @@ export async function GET(request: NextRequest) {
       response.headers.set(h, v);
     }
   }
+  const contentDisposition = `attachment; filename*=UTF-8''${encodeURIComponent(
+    nameWithExt
+  )}`;
+  try {
+    response.headers.set('Content-Disposition', contentDisposition);
+  } catch (err) {
+    throw err;
+  }
+
   return response;
 }
